88fafdc8eca7a45a9d138dfdf232cba2dedce6ca
Vikas Bhalla <vikas@ellingsentechnologies.com>
2024-05-05 17:05:53 +0530

    server file changes

--- app/configurations/config.ts
+++ app/configurations/config.ts
@@ -3,13 +3,6 @@ import path from "path"
 import * as dotenv from "dotenv"
 dotenv.config({ path: path.join(__dirname, "../../.env") })
 
-const MONGO_OPTIONS = {
-    useNewUrlParser: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-}
-
 const MONGO_USERNAME = process.env.MONGO_USERNAME || ""
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ""
 const MONGO_HOST = process.env.MONGO_URL || ""
@@ -18,7 +11,6 @@ const MONGO = {
     host: MONGO_HOST,
     username: MONGO_USERNAME,
     password: MONGO_PASSWORD,
-    options: MONGO_OPTIONS,
     url:
         MONGO_USERNAME !== "" && MONGO_PASSWORD !== ""
             ? `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}`
@@ -40,22 +32,12 @@ const JWT = {
     refreshTokenExpiresIn: process.env.REFRESH_TOKEN_LIFE,
 }
 
-const verifyEmailOnSignupServerUrl =
-    process.env.VERIFY_EMAIL_ON_SIGNUP_SERVER_URL || ""
-
-const routePermission = {
-    onlyAdmin: ["admin"],
-    onlyUser: ["user"],
-}
-
 const server_url = process.env.SERVER_URL || ""
 
 const config = {
     server: SERVER,
     mongo: MONGO,
     jwt: JWT,
-    verifyEmailOnSignupServerUrl,
-    routePermission,
     server_url,
 }
 
--- app/server.ts
+++ app/server.ts
@@ -1,70 +1,35 @@
 import http from "http"
 
 import bodyParser from "body-parser"
-// import cors from "cors"
 import express, { Request, Response } from "express"
 import mongoose from "mongoose"
 import passport from "passport"
 import config from "./configurations/config"
 import strategy from "./configurations/passport"
-import { isAuthorizedUser } from "./middlewares/auth.middleware"
 import authRouter from "./routes/auth.route"
 import userRouter from "./routes/user.route"
-// import authRouter from "./routes/auth.passport.route"
 
-const NAMESPACE = "Campr Server"
+const NAMESPACE = "Admin Server"
 const app = express()
 
-// set the view engine to ejs
-app.set('view engine', 'ejs')
-
-app.get('/T&C', (req, res) => {
-    res.render('t&c.ejs')
-})
 
 /* passport middleware */
 passport.use(strategy)
-app.get("/middleware", isAuthorizedUser, (req, res) => {
-    res.send("Route is open")
-})
 /* passport middleware */
 
 /** Connect to Mongo */
 mongoose.set("strictQuery", true)
 /** Connect to Mongo */
 mongoose
-    .connect(config.mongo.url /* , config.mongo.options */) // useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options
+    .connect(config.mongo.url)
     .then(() => {
-        console.log(NAMESPACE, "Campr DB Connected")
+        console.log(NAMESPACE, "Admin DB Connected")
     })
     .catch((error: any) => {
         console.error(NAMESPACE, error.message, error)
     })
 mongoose.Promise = global.Promise
 
-// app.use(
-//     cors({
-//         origin: (origin: any, callback: any) => {
-//             const originsQuery = SupportedOriginModel.find({}, "url").exec()
-//             originsQuery
-//                 .then((result: any) => {
-//                     const allowedOrigins = result.flatMap((o: any) => {
-//                         return o.url
-//                     })
-//                     allowedOrigins.push("http://localhost:3000")
-//                     allowedOrigins.push("https://campr-admin-staging.vercel.app")
-//                     allowedOrigins.push("https://campr-staging.vercel.app") // Add localhost to allowed origins
-//                     allowedOrigins.push(
-//                         "https://campr-frontend-git-staging-ellingsen-platforms.vercel.app"
-//                     )
-//                     callback(null, allowedOrigins)
-//                 })
-//                 .catch((error: any) => {
-//                     callback(new Error(error))
-//                 })
-//         },
-//     })
-// )
 
 /** Log the request */
 app.use((req, res, next) => {
